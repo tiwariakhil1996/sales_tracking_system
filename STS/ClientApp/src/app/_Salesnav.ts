410608897e8f66fbc3eefc73e709fd22b2b95887
vds786 <vikass@keypress.co.in>
2020-01-20 18:00:26 +0530

    nav and default page

---
+++ STS/ClientApp/src/app/_Salesnav.ts
@@ -0,0 +1,71 @@
+import { INavData } from '@coreui/angular';
+
+export const SalesnavItems: INavData[] = [
+
+  {
+    name: 'Dashboard',
+    url: '/sales/dashboard',
+    icon: 'icon-speedometer',
+    // badge: {
+    //   variant: 'info',
+    //   text: 'NEW'
+    // }
+  },
+  // {
+  //   title: true,
+  //   name: 'Forms'
+  // },
+  {
+    name: 'Add Product',
+    url: '/sales/product',
+    icon: 'icon-note'
+  },
+  {
+    name: 'Add Client',
+    url: '/sales/client',
+    icon: 'icon-user-follow'
+  },
+  // {
+  //   name: 'Assign List',
+  //   url: '/assign',
+  //   icon: 'icon-list'
+  // },
+  {
+    name: 'Activity',
+    url: '/sales/activity',
+    icon: 'icon-layers',
+    children: [
+      {
+        name: 'Add Activity',
+        url: '/sales/activity/addactivity',
+        icon: 'icon-note',
+      },
+      {
+        name: 'Past Activity',
+        url: '/sales/activity/pastactivity',
+        icon: 'icon-calendar',
+      },
+      {
+        name: 'Current Activity',
+        url: '/sales/activity/currentactivity',
+        icon: 'icon-notebook',
+      },
+    ]
+  },
+  {
+    name: 'Map',
+    url: '/sales/map',
+    icon: 'icon-map'
+  },
+  {
+    name: 'Demo',
+    url: '/sales/demo',
+    icon: 'icon-demo'
+  },
+  {
+    name: 'Logout',
+    url: '/sales/login',
+    icon: 'icon-logout'
+  }
+
+];
